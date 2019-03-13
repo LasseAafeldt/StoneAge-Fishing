077e8dae34c5a942ac36566a60f5ff873936387c
LasseAafeldt <superlla@hotmail.com>
2019-03-13 12:37:29 +0100

    disabled shadows for directional light cleaned timer script

--- MED7 - Stoneage Project/Assets/Scripts/playTimer.cs
+++ MED7 - Stoneage Project/Assets/Scripts/playTimer.cs
@@ -13,13 +13,15 @@ public class playTimer : MonoBehaviour {
 	Vector3 currentAngle = new Vector3(0,0,0);
 
 	public float totalPlayTime =300;
-	float time;
+	float timeLeft;
 	float timeSpent=0;
 
 	public Material skyBox;
-
+    [Range(345f,380f)]
 	public float skyboxAngle;
+    [Range(0.5f,1f)]
 	public float skyboxExposure;
+    public Light lightSource;
 
 	// 360 -> 345
 
@@ -33,7 +35,7 @@ public class playTimer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	time = totalPlayTime;
+	timeLeft = totalPlayTime;
 
 		Vector3 startAngle = new Vector3(startValue,23,3);	
 		Vector3 endAngle = new Vector3(endValue,0,0);
@@ -42,100 +44,118 @@ public class playTimer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time =  time - Time.deltaTime;
-		timeSpent = time/totalPlayTime;
+		timeLeft -= Time.deltaTime;
+		timeSpent = timeLeft/totalPlayTime;
 
-		skyBox.SetFloat("_RotationZ", skyboxAngle);
-		skyBox.SetFloat("_Exposure", skyboxExposure);
+        UpdateSkybox();
+        RotateSkybox();
 
-		transform.rotation = Quaternion.Euler(startAngle);
+        //EnableMiddenCollider();
 
-		//Debug.Log(timeSpent);
-		//Debug.Log(currentAngle.x);
-		if(timeSpent > 0)
-		{
-			skyboxAngle = (timeSpent)*380 + (1-timeSpent)*345;
-			skyboxExposure = (timeSpent)*(float)1 + (1-timeSpent)*(float)0.5;
-			currentAngle.x = (timeSpent)*startValue + (1-timeSpent)*endValue;
-			GetComponentInChildren<Light>().intensity=(timeSpent)*(float)1.2;
-		}
-		else
-		{
-			currentAngle.x = endValue;
-			GetComponentInChildren<Light>().intensity=0;
-		}
-
-		//transform.rotation = Quaternion.Lerp(Quaternion.Euler(startAngle),Quaternion.Euler(endAngle), timeSpent);
-		//transform.eulerAngles = new Vector3(currentAngle.x, 0, 0);
-		//Debug.Log(currentAngle.x);
-		transform.eulerAngles = new Vector3 (currentAngle.x,23, 3);
-
-		//Quaternion transform.Euler(currentAngle.x,0f,0f);
-		if(Input.GetKey("m"))
-		{
-			Debug.Log("midden");
-			GameManager.singleton.midden.GetComponent<Collider>().enabled = true;
-		}
-
-		if(timeSpent < 0)
-		{
-			// change scene
-			Debug.Log("change scene");
-			//did not make it back in time
-			if (GameManager.singleton.GetFishCount() >= 7)
-			{
-				GameManager.singleton.boat.GetComponent<EventCatcher>().CheckForEnding();
-			}
-			else
-			{
-				GameManager.singleton.PrepareForEndScene(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().Outcome2Emergent, 
-					GameManager.singleton.boat.GetComponent<EventCatcher>().GetHasFlint());
-				SceneManager.LoadScene("End Scene", LoadSceneMode.Single);
-			}
-		}
-		if(!GameManager.singleton.Islinear)
-		{
-			
-			if(timeSpent <=0.75 && sixMinLeft)
-			{
-				sixMinLeft=false;
-				GameManager.singleton.partner.
-					GetComponent<PartnerSpeech>().PartnerSaysSomething(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().WeNeedFish);
-				GameManager.singleton.partner.
-					GetComponent<PartnerSpeech>().PartnerSaysSomething(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().StartofGameEmergent);
-			}
-			if(timeSpent <=0.5 && fourMinLeft)
-			{
-				fourMinLeft=false;
-				GameManager.singleton.partner.
-					GetComponent<PartnerSpeech>().PartnerSaysSomething(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().DarkSoon);
-			}
-			//when there is two minutes left
-			if(timeSpent <=0.4 && twoMinLeft)
-			{
-				twoMinLeft=false;
-				GameManager.singleton.partner.
-					GetComponent<PartnerSpeech>().PartnerSaysSomething(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().Time2MinLeft);
-				GameManager.singleton.midden.GetComponent<Collider>().enabled = true;
-			}
-			//when there is one minutes left
-			if(timeSpent <=0.2 && oneMinLeft)
-			{
-				oneMinLeft =  false;
-				GameManager.singleton.partner.
-					GetComponent<PartnerSpeech>().PartnerSaysSomething(
-					GameManager.singleton.partner.GetComponent<PartnerSpeech>().Time1MinLeft);
-			}
-		}
+        RunOutOfTime();		
+        //IfLinearScene();
 	}
+
+    void UpdateSkybox()
+    {
+        if (timeSpent > 0)
+        {
+            skyboxAngle = (timeSpent) * 380 + (1 - timeSpent) * 345;
+            skyboxExposure = (timeSpent) * (float)1 + (1 - timeSpent) * (float)0.5;
+            currentAngle.x = (timeSpent) * startValue + (1 - timeSpent) * endValue;
+            //GetComponentInChildren<Light>().intensity=(timeSpent)*(float)1.2;
+            lightSource.intensity = (timeSpent) * (float)1.2;
+        }
+        else
+        {
+            currentAngle.x = endValue;
+            lightSource.intensity = 0;
+        }
+    }
+
+    void RotateSkybox()
+    {
+        skyBox.SetFloat("_RotationZ", skyboxAngle);
+        skyBox.SetFloat("_Exposure", skyboxExposure);
+
+        //transform.rotation = Quaternion.Euler(startAngle);
+        transform.eulerAngles = new Vector3(currentAngle.x, 23, 3);
+    }
+
+    void EnableMiddenCollider()
+    {
+        if (Input.GetKey("m"))
+        {
+            Debug.Log("midden");
+            GameManager.singleton.midden.GetComponent<Collider>().enabled = true;
+        }
+    }
+
+    void RunOutOfTime()
+    {
+        if (timeLeft < 0)
+        {
+            // change scene
+            Debug.Log("change scene");
+            //did not make it back in time
+            if (GameManager.singleton.GetFishCount() >= 7)
+            {
+                GameManager.singleton.boat.GetComponent<EventCatcher>().CheckForEnding();
+            }
+            else
+            {
+                GameManager.singleton.PrepareForEndScene(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().Outcome2Emergent,
+                    GameManager.singleton.boat.GetComponent<EventCatcher>().GetHasFlint());
+                SceneManager.LoadScene("End Scene", LoadSceneMode.Single);
+            }
+        }
+    }
 
 	public float GetTimeSpent()
 	{
 		return timeSpent;
 	}
+
+    void IfLinearScene()
+    {
+        if (!GameManager.singleton.Islinear)
+        {
+
+            if (timeSpent <= 0.75 && sixMinLeft)
+            {
+                sixMinLeft = false;
+                GameManager.singleton.partner.
+                    GetComponent<PartnerSpeech>().PartnerSaysSomething(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().WeNeedFish);
+                GameManager.singleton.partner.
+                    GetComponent<PartnerSpeech>().PartnerSaysSomething(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().StartofGameEmergent);
+            }
+            if (timeSpent <= 0.5 && fourMinLeft)
+            {
+                fourMinLeft = false;
+                GameManager.singleton.partner.
+                    GetComponent<PartnerSpeech>().PartnerSaysSomething(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().DarkSoon);
+            }
+            //when there is two minutes left
+            if (timeSpent <= 0.4 && twoMinLeft)
+            {
+                twoMinLeft = false;
+                GameManager.singleton.partner.
+                    GetComponent<PartnerSpeech>().PartnerSaysSomething(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().Time2MinLeft);
+                GameManager.singleton.midden.GetComponent<Collider>().enabled = true;
+            }
+            //when there is one minutes left
+            if (timeSpent <= 0.2 && oneMinLeft)
+            {
+                oneMinLeft = false;
+                GameManager.singleton.partner.
+                    GetComponent<PartnerSpeech>().PartnerSaysSomething(
+                    GameManager.singleton.partner.GetComponent<PartnerSpeech>().Time1MinLeft);
+            }
+        }
+    }
 }
