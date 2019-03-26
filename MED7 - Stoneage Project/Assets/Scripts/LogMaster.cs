4f53497bf06a068344c686984fe43aa14fe5cee0
LasseAafeldt <superlla@hotmail.com>
2019-03-26 12:39:56 +0100

    log structure should be ready for implementation now

--- MED7 - Stoneage Project/Assets/Scripts/LogMaster.cs
+++ MED7 - Stoneage Project/Assets/Scripts/LogMaster.cs
@@ -5,35 +5,70 @@ using System.IO;
 
 public class LogMaster : MonoBehaviour {
 
-    public static string path;
+    public static string filePath;
+    public string seperator = ",";
+
+    public Transform player;
+    public PartnerSpeech partnerSpeech;
+    public SelectTool selectTool;
+    //something variable where interactions are called
 
     void Start()
     {
-        createFile();
-        Debug.Log("I logged something");
+        //createFile();
+        //Debug.Log("I logged something");
     }
     void setPath()
     {
-        path = Application.persistentDataPath + "/" + System.DateTime.Now + ".txt";
+        filePath = Application.persistentDataPath + "/" + System.DateTime.Now.ToString() + ".txt";
     }
 
+    //find some way to create file only once while playing
     public void createFile()
     {
         setPath();
-        if (!File.Exists(path))
-        {
-            //File.WriteAllText(path, "I sucessfully wrote this...");
-            //File.OpenWrite(path);
-            FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+        if (!File.Exists(filePath))
+        {            
+            FileStream file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
             file.Close();
         }
     }
 
-    public void logEntry()
+    public void logEntry(int amountOfVoicelinesPlayed, Vector3 playerPosition, bool triedTorsk, bool triedEel, bool triedEelTrap, bool triedFlatfish, int totalFishCaught, string attemptedInteraction, int timesWrongToolHasBeenSelected)
     {
-        FileStream file = new FileStream(path, FileMode.Open, FileAccess.Write, FileShare.None);
-        //write the log stuff...
-        Debug.Log("Writing something in the log file");
-        file.Close();
+        using (StreamWriter SW = new StreamWriter(File.Open(filePath, FileMode.Append, FileAccess.Write, FileShare.Write)))
+        {
+            Debug.Log("the file is being written in now...");
+            //write the log stuff...
+            //remember system.DateTime first
+            string dateAndTime = System.DateTime.Now.ToString() + seperator;
+            string _amountOfVoiceLinesPlayed = amountOfVoicelinesPlayed.ToString() + seperator;
+            string _playerPosition = playerPosition.ToString() + seperator;
+            string _triedTorsk = triedTorsk.ToString() + seperator;
+            string _triedEel = triedEel.ToString() + seperator;
+            string _triedEeltrap = triedEelTrap.ToString() + seperator;
+            string _triedFlatfish = triedFlatfish.ToString() + seperator;
+            string _totalFishCaught = totalFishCaught.ToString() + seperator;
+            string _attemptedInteraction = attemptedInteraction.ToString() + seperator;
+            string _timesWrongTool = timesWrongToolHasBeenSelected.ToString();
+
+            string[] stuffToWrite = {
+                dateAndTime,
+                _amountOfVoiceLinesPlayed,
+                _playerPosition,
+                _triedTorsk,
+                _triedEel,
+                _triedEeltrap,
+                _triedFlatfish,
+                _totalFishCaught,
+                _attemptedInteraction,
+                _timesWrongTool
+            };
+            //string data = new string((string)dateAndTime + (string)_amountOfVoiceLinesPlayed + (string)_playerPosition + (string)_triedTorsk +(string) _triedEel + (string)_triedEeltrap + (string)_triedFlatfish + (string)_totalFishCaught + (string)_attemptedInteraction + (string)_timesWrongTool);
+            
+            SW.WriteLine(dateAndTime + _amountOfVoiceLinesPlayed + _playerPosition + _triedTorsk + _triedEel + _triedEeltrap + _triedFlatfish + _totalFishCaught + _attemptedInteraction + _timesWrongTool);
+
+            Debug.Log("writing in the file has been completed");            
+        }
     }
 }
