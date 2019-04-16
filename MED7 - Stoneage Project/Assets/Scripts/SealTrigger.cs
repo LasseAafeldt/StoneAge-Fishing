62ead012817868e8a27844db59b8c790e684cdac
rebeccapkt <rebecca@toft.cc>
2019-04-16 10:48:25 +0200

    fixed seal trigger

---
+++ MED7 - Stoneage Project/Assets/Scripts/SealTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealTrigger : MonoBehaviour {
+    PartnerSpeech ps;
+    bool hasPlayed = false;
+
+    private void Start()
+    {
+        ps = GameManager.singleton.partner.GetComponent<PartnerSpeech>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "boat")
+        {
+            return;
+        }
+
+        if (!hasPlayed)
+        {
+            ps.PartnerSaysSomething(ps.SealAppearsEmergent);
+            hasPlayed = true;
+        }
+    }
+}
