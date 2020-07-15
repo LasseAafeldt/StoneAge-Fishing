bb939aa084c6d3be8242bc5ab092827b5039d694
Heraji <jakob.hust@gmail.com>
2020-07-15 12:37:43 +0200

    Update PhoneIdle.cs

--- MED7 - Stoneage Project/Assets/Scripts/IdleManager/PhoneIdle.cs
+++ MED7 - Stoneage Project/Assets/Scripts/IdleManager/PhoneIdle.cs
@@ -5,36 +5,59 @@
 
     public class PhoneIdle : MonoBehaviour
     {
-        public float timeOut = 10;
+        [Tooltip("IF FALSE: for now uses rigidbody velocity of boat - But should be based on something else")] public bool usePhoneAccelerator = true;
+        public float timeOut = 30;
 
         private bool timerRunning, deviceIdle;
         private WaitForSeconds timerTime;
         private FadeController fadeController;
+        private Rigidbody boatRigidBody;
 
         #if UNITY_ANDROID
         void Start()
         {
             timerTime = new WaitForSeconds(timeOut);
             fadeController = FindObjectOfType<FadeController>();
+
+            if (!usePhoneAccelerator)
+                boatRigidBody = GameObject.FindGameObjectWithTag("boat")?.GetComponent<Rigidbody>();
         }
 
-        void Update()
+        void FixedUpdate()
         {
-            Controller(Input.acceleration.magnitude);
+            if (!usePhoneAccelerator)
+            {
+                if (boatRigidBody != null)
+                    Controller(boatRigidBody.velocity.magnitude);
+            }
+            else
+                Controller(Input.acceleration.magnitude);
         }
 
         private void Controller(float accelerationAmount)
         {
-            if (deviceIdle) return;
-
-            if (accelerationAmount < 8.0f)
-                StartCoroutine(Timer());
+            if (accelerationAmount < 0.2f)
+            {
+                if (!deviceIdle && !timerRunning)
+                    StartCoroutine(Timer());
+            }
             else if (timerRunning)
             {
                 StopAllCoroutines();
                 timerRunning = false;
-                fadeController?.fadeIn();
+                FadeIn();
             }
+            else
+                FadeIn();
+        }
+
+        private void FadeIn()
+        {
+            if (!deviceIdle)
+                return;
+
+            deviceIdle = false;
+            fadeController?.fadeIn();
         }
 
         IEnumerator Timer()
@@ -42,6 +65,7 @@
             timerRunning = true;
             yield return timerTime;
 
+            deviceIdle = true;
             fadeController?.fadeOut();
         }
         #endif
