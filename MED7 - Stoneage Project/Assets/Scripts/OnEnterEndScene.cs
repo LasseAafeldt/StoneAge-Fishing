ba21effd59268d0ca0c4c4cb1e3f8a21d3afab60
LasseAafeldt <superlla@hotmail.com>
2020-10-06 09:48:02 +0200

    end scene sound now plays

---
+++ MED7 - Stoneage Project/Assets/Scripts/OnEnterEndScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnterEndScene : MonoBehaviour
+{
+    private AudioSource sound;
+
+    private void Start()
+    {
+        sound = GameManager.singleton._audio;
+        sound.Play();
+    }
+}
