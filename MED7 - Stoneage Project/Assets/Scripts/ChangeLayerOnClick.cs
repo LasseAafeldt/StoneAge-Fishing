4ece71e9254ba452a5a35aa57dd39bdec04d999a
noesmos <tnm@live.dk>
2020-10-07 15:24:01 +0200

    tutorial videos added

---
+++ MED7 - Stoneage Project/Assets/Scripts/ChangeLayerOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLayerOnClick : MonoBehaviour
+{
+    [SerializeField] private int maxClicks = 1;
+
+    private int currentClicks = 0;
+
+    public void  CountUp()
+    {
+        currentClicks++;
+        if (currentClicks >=maxClicks)
+        {
+
+        }
+    }
+
+    private void ChangeLayer()
+    {
+        gameObject.layer = "default";
+    }
+
+
+}
---
+++ MED7 - Stoneage Project/Assets/Scripts/VideoPlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerManager : MonoBehaviour
+{
+
+    public float yieldTime =2;
+
+    [SerializeField] private VideoControls videoControls;
+
+    private void Awake()
+    {
+        if (!videoControls)
+        {
+            videoControls = GetComponentInChildren<VideoControls>();
+        }
+       
+    }
+
+    public void StartVideoWithDelay()
+    {
+        StartCoroutine(StartVideoCoroutine(yieldTime));
+    }
+
+    IEnumerator StartVideoCoroutine(float yieldTime)
+    {
+        yield return new WaitForSeconds(yieldTime);
+        videoControls.gameObject.SetActive(true);
+        videoControls.PlayWithEndEvent();
+    }
+}
