0f7ad1082eb3d2a1bb49b61741c93710873da787
noesmos <tnm@live.dk>
2020-10-09 12:16:25 +0200

    calibration works + delay call tool implemented

---
+++ MED7 - Stoneage Project/Assets/Scripts/MatchHorizontalDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchHorizontalDirection : MonoBehaviour
+{
+    public Transform matchObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (matchObject)
+        {
+            SetRotation(matchObject);
+        }
+    }
+
+    void SetRotation(Transform target)
+    {
+        Vector3 degrees = target.eulerAngles;
+
+        Vector3 onlyY = new Vector3(0, degrees.y, 0);
+
+        transform.eulerAngles = onlyY;
+    }
+}
