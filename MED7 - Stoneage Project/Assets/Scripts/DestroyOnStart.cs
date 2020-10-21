17d70f12304ff8f88db11e6510b53eb9d080388b
LasseAafeldt <superlla@hotmail.com>
2020-10-21 15:52:00 +0200

    highlight fix and start of remove gamamanager from pause scene

---
+++ MED7 - Stoneage Project/Assets/Scripts/DestroyOnStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnStart : MonoBehaviour
+{
+    [SerializeField] private string nameOfTagToDestroy;
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("nameOfTagToDestroy");
+        if (objs.Length > 0)
+        {
+            foreach (GameObject obj in objs)
+            {
+                Debug.Log("Destroying " + nameOfTagToDestroy);
+                Destroy(obj);
+            }
+        }
+#if UNITY_EDITOR
+        Debug.LogError("Tag you are trying to find does not exist ", gameObject);         
+#endif
+    }
+}
