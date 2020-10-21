a182258c50e1f881ce64c8d556be23d1f8f99d56
noesmos <tnm@live.dk>
2020-10-21 09:46:22 +0200

    new pause screen

--- MED7 - Stoneage Project/Assets/Scripts/PauseDetectionAndHandling/TrackButtonInput.cs
+++ MED7 - Stoneage Project/Assets/Scripts/PauseDetectionAndHandling/TrackButtonInput.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(EventTrigger))]
 public class TrackButtonInput : MonoBehaviour, ITrackActivity
 {
 
