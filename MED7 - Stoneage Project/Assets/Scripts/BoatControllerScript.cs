f8fcaf015ace761a7560129cbc22b0df90605181
LasseAafeldt <superlla@hotmail.com>
2020-10-29 09:59:58 +0100

    Update BoatControllerScript.cs

--- MED7 - Stoneage Project/Assets/Scripts/BoatControllerScript.cs
+++ MED7 - Stoneage Project/Assets/Scripts/BoatControllerScript.cs
@@ -8,16 +8,22 @@ public class BoatControllerScript : MonoBehaviour
     public float speed = 15f, rotationSpeed = 0.1f, rotationDeadzone = 5f, accelerationAmount = 0.05f, accelerationDeadzone = 90f, rowingFrequency = 0.44f;
     public Collider[] ignoreCollision;
 
-    bool voiceLineReady = true;
-
-    float verticalInput, horizontalInput, steerFactor, acceleration;
-
     private bool outOfBounds = false;
+    private bool rowFXPlaying = false;
+    private bool shouldMove = true;
+    private float acceleration;
+
+    private AudioSource paddleSource;
+    private Camera mainCamera;
+    private PartnerAnimator animator;
+    private Rigidbody boatRB;
 
     private void Start()
     {
         outOfBounds = false;
-        voiceLineReady = true;
+        rowFXPlaying = false;
+        shouldMove = true;
+        CacheComponents();
     }
 
     void Update()
@@ -30,43 +36,40 @@ public class BoatControllerScript : MonoBehaviour
 
     public void BoatMovement()
     {
-        if (Input.GetButtonDown("Fire1") && !outOfBounds && !GameManager.singleton.pointingAtInteractable && GameManager.singleton.canMove)
+        if (Input.GetButtonDown("Fire1") && !outOfBounds && !GameManager.singleton.pointingAtInteractable)
         {
-            GameManager.singleton.paddle.GetComponent<AudioSource>().Play();
-            GameManager.singleton.partner.GetComponent<PartnerAnimator>().paddleAnimation(true);
+            EnableSailing();
+            Debug.Log("I got here");
         }
         else if (Input.GetButtonUp("Fire1") && !outOfBounds)
         {
-            GameManager.singleton.paddle.GetComponent<AudioSource>().Stop();
-            GameManager.singleton.partner.GetComponent<PartnerAnimator>().paddleAnimation(false);
+            DisableSailing();
         }
 
         if (Input.GetButton("Fire1") && !outOfBounds)
         {
-            //float sinusoid = (Mathf.Sin(Time.time) + 1.0f / 2.0f);
             float sinusoid = (Mathf.Sin(2.0f * Mathf.PI * rowingFrequency * Time.time+1) + 1.0f / 2.0f); // Asin(2 * PI * Frequency * time) + phase
             if (sinusoid < 0.25f)
-            {
                 sinusoid = 0.25f;
-            }
 
             if (acceleration < 1.0f)
                 acceleration += accelerationAmount;
 
             if (!GameManager.singleton.pointingAtInteractable)
             {
-                float rotationAngle = Vector3.Angle(transform.forward, Camera.main.transform.forward);
+                float rotationAngle = Vector3.Angle(transform.forward, mainCamera.transform.forward);
 
-                if (GameManager.singleton.partner.GetComponent<PartnerAnimator>().anim.GetBool("isRowing") && rotationAngle < accelerationDeadzone)
+                if (animator.anim.GetBool("isRowing") && rotationAngle < accelerationDeadzone)
                 {
-                    GetComponent<Rigidbody>()
+                    boatRB
                         .AddForce((transform.forward * speed * sinusoid * Time.deltaTime) * acceleration);
+                    Debug.Log("Should row forwards    " + Time.time);
                 }
 
                 if (rotationAngle > rotationDeadzone)
                 {
                     Vector3 newDir = Vector3.RotateTowards(transform.forward,
-                        new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z), rotationAngle,
+                        new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z), rotationAngle,
                         0.0f);
 
                     // calculate the Quaternion for the rotation
@@ -117,5 +120,29 @@ public class BoatControllerScript : MonoBehaviour
         //set curentregion to no region
         currentlyInRegion = "No Region";
         //if multiple colliders is use to make one region, then check if other region colliders overlap with current position
+    }
+
+    private void EnableSailing()
+    {
+        rowFXPlaying = true;
+        paddleSource.Play();
+        animator.paddleAnimation(true);
+        shouldMove = true;
+    }
+
+    private void DisableSailing()
+    {
+        rowFXPlaying = false;
+        paddleSource.Stop();
+        animator.paddleAnimation(false);
+        shouldMove = false;
+    }
+
+    private void CacheComponents()
+    {
+        paddleSource = GameManager.singleton.paddle.GetComponent<AudioSource>();
+        animator = GameManager.singleton.partner.GetComponent<PartnerAnimator>();
+        mainCamera = Camera.main;
+        boatRB = GetComponent<Rigidbody>();
     }
 }
